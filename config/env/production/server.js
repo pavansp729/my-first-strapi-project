6765e6ab039f0b2dd06863617f8c1486096269e3
pavansp729 <pavansp729@gmail.com>
2026-01-27 00:59:17 +0530

    commit message

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 
     module.exports = ({ env }) => ({
         proxy: true,
-        url: env('mt-strapi.pavansprabhu.in'), // Sets the public URL of the application.
+        url: env('my-strapi.pavansprabhu.in'), // Sets the public URL of the application.
         app: { 
           keys: env.array('APP_KEYS')
         },
